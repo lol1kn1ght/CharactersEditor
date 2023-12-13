d1d6ec6bab230be0511470319ef9f82f3a88a4b8
lol1kn1ght <loliknightez@gmail.com>
2023-12-13 20:53:56 +0300

    refactor: убрал заглушку

--- src/App.tsx
+++ src/App.tsx
@@ -2,52 +2,8 @@ import { Navigate, Route, Routes } from 'react-router-dom'
 import { Home } from './pages/Home'
 import { NotFound } from './pages/NotFound'
 import './App.scss'
-import { useEffect } from 'react'
-import { useCharactersStore } from './store'
-import { Character } from './entities/Character'
 
 export const App = () => {
-  const store = useCharactersStore()
-
-  useEffect(() => {
-    store.setCharacters([
-      new Character('LOlik1').editParameters({
-        power: 10,
-      }),
-      new Character('abobki'),
-      new Character(`${Math.floor(Math.random() * 10000)}`),
-      new Character(`${Math.floor(Math.random() * 10000)}`),
-      new Character(`${Math.floor(Math.random() * 10000)}`),
-      new Character(`${Math.floor(Math.random() * 10000)}`),
-      new Character(`${Math.floor(Math.random() * 10000)}`),
-      new Character(`${Math.floor(Math.random() * 10000)}`),
-      new Character(`${Math.floor(Math.random() * 10000)}`),
-      new Character(`${Math.floor(Math.random() * 10000)}`),
-      new Character(`${Math.floor(Math.random() * 10000)}`),
-      new Character(`${Math.floor(Math.random() * 10000)}`),
-      new Character(`${Math.floor(Math.random() * 10000)}`),
-      new Character(`${Math.floor(Math.random() * 10000)}`),
-      new Character(`${Math.floor(Math.random() * 10000)}`),
-      new Character(`${Math.floor(Math.random() * 10000)}`),
-      new Character(`${Math.floor(Math.random() * 10000)}`),
-      new Character(`${Math.floor(Math.random() * 10000)}`),
-      new Character(`${Math.floor(Math.random() * 10000)}`),
-      new Character(`${Math.floor(Math.random() * 10000)}`),
-      new Character(`${Math.floor(Math.random() * 10000)}`),
-      new Character(`${Math.floor(Math.random() * 10000)}`),
-      new Character(`${Math.floor(Math.random() * 10000)}`),
-      new Character(`${Math.floor(Math.random() * 10000)}`),
-      new Character(`${Math.floor(Math.random() * 10000)}`),
-      new Character(`${Math.floor(Math.random() * 10000)}`),
-      new Character(`${Math.floor(Math.random() * 10000)}`),
-      new Character(`${Math.floor(Math.random() * 10000)}`),
-      new Character(`${Math.floor(Math.random() * 10000)}`),
-      new Character(`${Math.floor(Math.random() * 10000)}`),
-      new Character(`${Math.floor(Math.random() * 10000)}`),
-      new Character(`${Math.floor(Math.random() * 10000)}`),
-    ])
-  }, [])
-
   return (
     <div className="app">
       <Routes>
