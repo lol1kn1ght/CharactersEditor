42e2c4f7a51ec2cda07c63efb465031ed08ee006
lol1kn1ght <loliknightez@gmail.com>
2023-12-13 18:57:12 +0300

    feat: добавил отображение активной ссылки

--- src/pages/Home/components/Navigation/Navigation.tsx
+++ src/pages/Home/components/Navigation/Navigation.tsx
@@ -1,22 +1,45 @@
 import { Link } from 'react-router-dom'
 import './Navigation.scss'
+import { useState } from 'react'
 
 export const Navigation = () => {
+  const [current, setCurrent] = useState<string>('/')
+
+  const routes: { to: string; name: string }[] = [
+    {
+      name: 'Персонажи',
+      to: '/',
+    },
+    {
+      name: 'Редактор',
+      to: '/editor',
+    },
+    {
+      name: 'Иморт',
+      to: '/import',
+    },
+    {
+      name: 'Экспорт',
+      to: '/export',
+    },
+  ]
+
+  const onClick = (path: string) => {
+    setCurrent(path)
+  }
+
   return (
     <header className="_navigation">
       <div className="controls">
-        <Link to="/characters" className="control">
-          Персонажи
-        </Link>
-        <Link to="/editor" className="control">
-          Редактор
-        </Link>
-        <Link to="/import" className="control">
-          Импорт
-        </Link>
-        <Link to="/export" className="control">
-          Экспорт
-        </Link>
+        {routes.map((route) => (
+          <Link
+            to={route.to}
+            className={`control ${route.to === current ? 'active' : ''}`}
+            onClick={() => onClick(route.to)}
+          >
+            {route.name}
+          </Link>
+        ))}
       </div>
     </header>
   )
